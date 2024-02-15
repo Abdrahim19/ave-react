3481882a4426ae61db58af85d11e5f12ef900837
hamzaAbdrahim <rsstrr7@gmail.com>
2024-02-15 14:26:58 +0100

    'push-site'

---
+++ src/types/Types.ts
@@ -0,0 +1,15 @@
+export type reviweCard = {
+    id:number;
+    userImg:string;
+    userName:string;
+    userRate:number;
+    reviweDiscrption:string;
+    reviewPubliched:string;
+}
+export type carsoulCard = {
+    id:number;
+    userImg:string;
+    userName:string;
+    userJob:string;
+    userTages:string[];
+}
