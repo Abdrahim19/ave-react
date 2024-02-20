7fb2e916742c97c6e3ec452a5c8dfe506dcd86ee
hamzaAbdrahim <rsstrr7@gmail.com>
2024-02-20 16:18:13 +0100

    'push_site'

--- src/layout/MessagesLayout.tsx
+++ src/layout/MessagesLayout.tsx
@@ -3,7 +3,7 @@ import  masgDots from "../assets/masgDots.svg";
 import  masg_user_img from "../assets/masg-user-img.png";
 import  blueAdd from "../assets/blue__add.png";
 import  blueSend from "../assets/blue__send.png";
-
+import "../pages/chat/chat.css";
 
 
 const MessagesLayout = () => {
@@ -14,47 +14,113 @@ const MessagesLayout = () => {
       <div className="row gap-4 gap-md-0 container-xl">
           <div className="col-md- col-lg-3">
             <div className="flex-items justify-content-between">
-              <h3 className="display-1 fw-medium">Messages</h3>
+              <h3 className="msg-titel">Messages</h3>
               <button>
                 <img src={masgDots} alt="" />
               </button>
             </div>
-            <div className="position-relative">
+            <div className="position-relative my-4">
               <input type="text" placeholder="Recherchez ici" className="msg-searchInput" />
-              <i className="bi bi-search"></i>
+              <i className="bi bi-search top-50 translate-middle-y"></i>
             </div>
             <div className="msg-user flex-items gap-3">
-              <img src={masg_user_img} className="masg_user_img" alt="" />
+              <div className="position-relative masg_user_img">
+              <img src={masg_user_img} className="img-fluid" alt="" />
+              <span className="online-dot position-absolute"></span>
+              </div>
+              <div>
+                <h3 className="masg-user-name">Kate Lingard</h3>
+                <p className="data-left">En ligne à 17h00</p>
+              </div>
+            </div>
+            <div className="msg-user flex-items gap-3">
+              <div className="position-relative masg_user_img">
+              <img src={masg_user_img} className="img-fluid" alt="" />
+              <span className="online-dot position-absolute"></span>
+              </div>
+              <div>
+                <h3 className="masg-user-name">Kate Lingard</h3>
+                <p className="data-left">En ligne à 17h00</p>
+              </div>
+            </div>
+            <div className="msg-user flex-items gap-3">
+              <div className="position-relative masg_user_img">
+              <img src={masg_user_img} className="img-fluid" alt="" />
+              <span className="online-dot position-absolute"></span>
+              </div>
+              <div>
+                <h3 className="masg-user-name">Kate Lingard</h3>
+                <p className="data-left">En ligne à 17h00</p>
+              </div>
+            </div>
+            <div className="msg-user flex-items gap-3">
+              <div className="position-relative masg_user_img">
+              <img src={masg_user_img} className="img-fluid" alt="" />
+              <span className="online-dot position-absolute"></span>
+              </div>
+              <div>
+                <h3 className="masg-user-name">Kate Lingard</h3>
+                <p className="data-left">En ligne à 17h00</p>
+              </div>
+            </div>
+            <div className="msg-user flex-items gap-3">
+              <div className="position-relative masg_user_img">
+              <img src={masg_user_img} className="img-fluid" alt="" />
+              <span className="online-dot position-absolute"></span>
+              </div>
               <div>
                 <h3 className="masg-user-name">Kate Lingard</h3>
                 <p className="data-left">En ligne à 17h00</p>
               </div>
             </div>
           </div>
-          <div className="col-md- col-lg-6">
-            <div className="msg-respons d-flex">
-              <img src={masg_user_img} className="verySmall_img" alt="" />
+          <div className="col-md- col-lg-6 mx-4 px-4 border-left-right position-relative">
+            <div className="msg-respons mb-4 d-flex gap-3">
+              <img src={masg_user_img} className="verySmall_img align-self-end" alt="" />
               <div className="msg-box">
                 <p className="msg">Hey! i would like to know what do you want to fix?</p>
                 <p className="dataSand__msg">13:01</p>
               </div>
             </div>
-            <div className="msg-sand d-flex">
-              <img src={masg_user_img} className="verySmall_img" alt="" />
+            <div className="msg-sand mb-4 text-end w-100 d-flex gap-3 align-self-end">
               <div className="sand_msg-box">
-                <p className="msg">Hey! i would like to know what do you want to fix?</p>
+                <p className="msg">
+                hello! Thanks for reaching out so there are many problems
+                 of Lorem ipsum so i was looking for someone to help
+                </p>
                 <div className="flex-items justify-content-between">
                 <p className="dataSand__msg">13:01</p>
                 <p className="timeMsg__sand">Seen 13:05</p>
                 </div>
               </div>
+              <img src={masg_user_img} className="verySmall_img align-self-end" alt="" />
             </div>
-            <div className="position-relative">
-              <input type="text" className="sand__input"/>
-              <div className="position-absolute top-50 translate-middle-y add__container">
+            <div className="msg-respons mb-4 d-flex gap-3">
+              <img src={masg_user_img} className="verySmall_img align-self-end" alt="" />
+              <div className="msg-box">
+                <p className="msg">Hey! i would like to know what do you want to fix?</p>
+                <p className="dataSand__msg">13:01</p>
+              </div>
+            </div>
+            <div className="msg-sand mb-4 text-end w-100 d-flex gap-3 align-self-end">
+              <div className="sand_msg-box">
+                <p className="msg">
+                hello! Thanks for reaching out so there are many problems
+                 of Lorem ipsum so i was looking for someone to help
+                </p>
+                <div className="flex-items justify-content-between">
+                <p className="dataSand__msg">13:01</p>
+                <p className="timeMsg__sand">Seen 13:05</p>
+                </div>
+              </div>
+              <img src={masg_user_img} className="verySmall_img align-self-end" alt="" />
+            </div>
+            <div className="sand__input__conatiner flex-items justify-content-between">
+              <div className="add__container cursor-pointer">
                 <img src={blueAdd} alt="" className="add--img" />
               </div>
-              <div className="position-absolute top-50 translate-middle-y sand__img">
+              <input type="text" className="sand__input w-100 mx-4" placeholder="Écrivez votre message ici"/>
+              <div className="sand__img cursor-pointer">
               <img src={blueSend} alt="" className="sand--img" />
               </div>
             </div>
