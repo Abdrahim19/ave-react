d2b3094063c464e6f743319a68d82dc8f262b26b
hamzaAbdrahim <rsstrr7@gmail.com>
2024-02-25 11:33:40 +0100

    'push_origin_main'

--- src/pages/home/hero/Hero.tsx
+++ src/pages/home/hero/Hero.tsx
@@ -2,12 +2,13 @@ import './hero.css'
 import hero_logo from "../../../assets/svg file 1.png";
 import Ellipse from "../../../assets/Ellipse 108.png";
 import pics from "../../../assets/pics.png";
+import SearchBar from './searchBar/SearchBar';
 
 const Hero = () => {
   return (
-    <main className="hero-section mt-5 container-lg position-relative cenetr-grid">
+    <main className="hero-section mt-3 container-lg position-relative cenetr-grid">
       <img src={Ellipse} className="position-absolute big-circal w-100 z-1" alt="" />
-      <div className="hero-background position-relative z-2 py-4">
+      <div className="hero-background position-relative z-2 p-2 p-md-4">
         <div className="row gap-4 gap-lg-0">
           <div className="col-lg-7">
             <div className="text-center text-lg-start">
@@ -15,9 +16,8 @@ const Hero = () => {
             </div>
             <h1 className="hero-titel text-center text-lg-start my-3">Prestations de <br className="d-none d-md-block" /> services et location de <br className="d-none d-md-block" /> matériel</h1>
             <p className="hero-discrption text-center text-lg-start">4 millions d’habitants et <br /> professionnels partout en France</p>
-            <div className="position-relative hero-input-container col-sm-10 col-md-6 col-lg-8 mx-auto mx-lg-0">
-              <input type="text" className="hero-input w-100 bg-transparent" placeholder="Que recherchez vous ?" />
-              <i className="bi bi-search top-50 translate-middle-y" style={{ position: 'absolute', right: '1rem' }}></i>
+            <div className=' col-sm-10 col-md-8 col-lg-11 mx-auto mx-lg-0'>
+            <SearchBar />
             </div>
             <div className="flex-items justify-content-center justify-content-lg-start mt-4" style={{ gap: 'clamp( 5px ,4vw , 10px)', marginLeft: '5px' }}>
               <p className="hero-text">Populair:</p>
--- src/pages/home/hero/searchBar/SearchBar.tsx
+++ src/pages/home/hero/searchBar/SearchBar.tsx
@@ -1,15 +1,22 @@
 import './searchBar.css'
 const SearchBar = () => {
   return (
-    <div className="bar">
-    <div className="location">
+    <div className="bar row p-2 align-items-center">
+    <div className="location col-5">
       <p className='searchBar_label'>Location</p>
       <input type="text" placeholder="Que vous voulez"  />
     </div>
-    <div className="check-in">
+    <div className="check-in col-5">
       <p className='searchBar_label'>Check in</p>
       <input type="text" placeholder="W l'emplacement" />
     </div>
+    <div className='col-2'>
+    <div className='search-container'>
+    <i className="bi bi-search"></i>
+    </div>
+    </div>
+    <span className='top-50 translate-middle-y col-lg-2' style={{ position: 'absolute', right: '1rem' }}>
+    </span>
   </div>
   )
 }
