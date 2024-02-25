c749157ab486ef71130d7588b0462b683f2fa8c5
hamzaAbdrahim <rsstrr7@gmail.com>
2024-02-25 15:24:20 +0100

    'push_origin_main'

--- src/pages/dashbord/ProposezCard/ProposezCard.tsx
+++ src/pages/dashbord/ProposezCard/ProposezCard.tsx
@@ -2,7 +2,7 @@ import './proposez.css'
 
 const ProposezCard = () => {
   return (
-    <div className="Proposez-card mt-4 mb-lg-0 mb-4">
+    <div className="Proposez-card mt-2 mt-xl-4 mb-lg-0 mb-4">
     <h1 className="Proposez-titel">Proposez vos services</h1>
     <p className="Proposez-discrption">Arrondissez vos fins de mois ou développez
          votre activité professionnelle, 
