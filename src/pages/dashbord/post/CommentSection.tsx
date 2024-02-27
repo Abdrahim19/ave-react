46aa06ff37af7d651f2e23c71a2d4fa8037dbe87
hamzaAbdrahim <rsstrr7@gmail.com>
2024-02-27 16:19:10 +0100

    'push_site'

--- src/pages/dashbord/post/CommentSection.tsx
+++ src/pages/dashbord/post/CommentSection.tsx
@@ -3,16 +3,17 @@ import { useYourContext } from "../../../context/MultiStepForm";
 import Comment from "./Comment";
 
 const CommentSection = () => {
-  const { toggleChatOpen , IsChatOpen} = useYourContext();
-    return (
-      <section className={`${IsChatOpen ? "active" : ""} comment__section`}>
+  const { toggleChatOpen, IsChatOpen } = useYourContext();
+
+  return (
+    <section className={`animate__animated ${IsChatOpen ? 'animate__backInDown' :  "animate__backOutDown h-transtion" } `}>
       <hr className="post-line" />
       <div className='flex-items gap-3 p-4 pb-2'>
         <div className='size-50px'>
           <img src="https://i.ibb.co/PTrBjrY/Image-40.png" className="img-fluid w-100 h-100" alt="" />
         </div>
-        <div className=" w-100">
-        <Inputlabel name={""} type={"text"} label={"What are you thinking?"} />
+        <div className="w-100">
+          <Inputlabel name={""} type={"text"} label={"What are you thinking?"} />
         </div>
       </div>
       <div className="flex-items justify-content-end gap-2 px-4 pb-4">
@@ -25,12 +26,11 @@ const CommentSection = () => {
           <i className="bi bi-send sp-letter-05"></i>
         </button>
       </div>
-      <h1 className="fw-fw-medium fs-px_18 px-4 clr-gray-300">See ALL comment</h1>
+      <h1 className="fw-fw-medium fs-px_18 px-4 clr-gray-300">See ALL comments</h1>
       <hr className="post-line" />
       <Comment />
-      </section>
-    );
-  }
-  
-  export default CommentSection;
-  
+    </section>
+  ) ;
+};
+
+export default CommentSection;
