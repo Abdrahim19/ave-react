4f7de75e36aed2522e94e2feef11a36a3b03b802
hamzaAbdrahim <rsstrr7@gmail.com>
2024-03-03 13:10:36 +0100

    push__some__updata

--- src/layout/DashbordLayout.tsx
+++ src/layout/DashbordLayout.tsx
@@ -1,15 +1,12 @@
 
-import { AddPostContext } from "../context/AddPostSteeperContext"
 import AccueilPopup from "../pages/dashbord/AccueilPopup/AccueilPopup"
 import ProposezCard from "../pages/dashbord/ProposezCard/ProposezCard"
 import AddPost from "../pages/dashbord/addPost/AddPost"
 import DemandeCard from "../pages/dashbord/dmaned/Dmanedcart"
 import Navbar from "../pages/dashbord/navbar/Navbar"
 import AllPosts from "../pages/dashbord/post/AllPosts"
-import AddPostSteeper from "../pages/home/addPostSteeper/AddPostSteeper"
 
 const DashbordLayout = () => {
-  const {showPostPopup} = AddPostContext()
   return (
     <>
     <Navbar />
@@ -28,7 +25,6 @@ const DashbordLayout = () => {
       </div>
   </div>
   <AccueilPopup />
-  {showPostPopup ? <AddPostSteeper /> : false}
     </>
   )
 }
--- src/pages/dashbord/navbar/Navbar.tsx
+++ src/pages/dashbord/navbar/Navbar.tsx
@@ -3,10 +3,15 @@ import logo  from '../../../assets/logo.png'
 import PopupInfo from './PopupInfo';
 import Headerlinks from './Headerlinks';
 import "./navbar.css";
+import { AddPostContext } from '../../../context/AddPostSteeperContext';
+import AddPostSteeper from '../../home/addPostSteeper/AddPostSteeper';
 
 
 const Navbar = () => {
+  const {showPostPopup} = AddPostContext()
     return (
+      <>
+  {showPostPopup ? <AddPostSteeper /> : false}
       <header className="home-header">
         <div className="container-md flex-items flex-wrap justify-content-between py-1">
           <Link to="/" className='home-logo'>
@@ -16,6 +21,7 @@ const Navbar = () => {
           <PopupInfo />
         </div>
       </header>
+      </>
     );
   };
   
