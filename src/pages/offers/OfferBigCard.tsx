2099dd2c89ea858563e7352ae4da30c79a84fb88
hamzaAbdrahim <rsstrr7@gmail.com>
2024-03-03 12:14:52 +0100

    push__some__updata

--- src/pages/offers/OfferBigCard.tsx
+++ src/pages/offers/OfferBigCard.tsx
@@ -1,28 +1,82 @@
-import { useState, useEffect } from "react";
-import { getAllUserOfferCard } from "../../service/apiService";
+// import { useState, useEffect } from "react";
+// import { getAllUserOfferCard } from "../../service/apiService";
 import Filter from "./filter/Filter";
 import UserOfferCard from "./userOffercard/UserOfferCard";
 import { UserOfferCardTypes } from "../../types/Types";
-import LoderUserOfferCard from "../../components/commen/sklitionsLoders/UserOfferCardLoder";
+// import LoderUserOfferCard from "../../components/commen/sklitionsLoders/UserOfferCardLoder";
 
 const OfferBigCard = () => {
-  const [userOfferCards, setUserOfferCards] = useState<UserOfferCardTypes[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchUserOfferCards = async () => {
-      try {
-        const userOfferCardData = await getAllUserOfferCard();
-        setUserOfferCards(userOfferCardData);
-      } catch (error) {
-        setLoading(false);
-        console.error('Error fetching user offer cards:', error);
-      } finally {
-        setLoading(false);
+  // const [userOfferCards, setUserOfferCards] = useState<UserOfferCardTypes[]>([]);
+  const userOfferCards:UserOfferCardTypes[] = [{
+    title: "Femme de Ménage",
+    description: "36 Particuliers et 4 Professionnels",
+    users: [
+      {
+        id: 1,
+        name: "Sophia",
+        rating: 4.3,
+        type: "Particulier",
+        online: true,
+        link: "/",
+        workingImages: [
+          "https://i.ibb.co/6rY6zNP/Rectangle-242.png",
+          "https://i.ibb.co/6rY6zNP/Rectangle-242.png"
+        ]
+      },
+      {
+        id: 2,
+        name: "Michael",
+        rating: 4.2,
+        type: "Professional",
+        online: true,
+        link: "/",
+        workingImages: [
+          "https://i.ibb.co/6rY6zNP/Rectangle-242.png",
+          "https://i.ibb.co/6rY6zNP/Rectangle-242.png"
+        ]
+      },
+      {
+        id: 3,
+        name: "Emma",
+        rating: 4.5,
+        type: "Particulier",
+        online: true,
+        link: "/",
+        workingImages: [
+          "https://i.ibb.co/6rY6zNP/Rectangle-242.png",
+          "https://i.ibb.co/6rY6zNP/Rectangle-242.png"
+        ]
+      },
+      {
+        id: 4,
+        name: "John",
+        rating: 4.7,
+        type: "Professional",
+        online: false,
+        link: "/",
+        workingImages: [
+          "https://i.ibb.co/6rY6zNP/Rectangle-242.png",
+          "https://i.ibb.co/6rY6zNP/Rectangle-242.png"
+        ]
       }
-    };
-    fetchUserOfferCards();
-  }, []);
+    ],
+  },]
+  // const [loading, setLoading] = useState<boolean>(true);
+
+  // useEffect(() => {
+  //   const fetchUserOfferCards = async () => {
+  //     try {
+  //       const userOfferCardData = await getAllUserOfferCard();
+  //       setUserOfferCards(userOfferCardData);
+  //     } catch (error) {
+  //       setLoading(false);
+  //       console.error('Error fetching user offer cards:', error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  //   fetchUserOfferCards();
+  // }, []);
 
   
   return (
@@ -31,18 +85,14 @@ const OfferBigCard = () => {
       <div className="col-md-12 col-lg-10 mx-auto">
         <Filter />
       </div>
-      {!loading ? (
-        userOfferCards.map((user) => (
-          <UserOfferCard
-            key={user.title} 
-            title={user.title}
-            description={user.description}
-            users={user.users}
-          />
-        ))
-      ) : (
-        <LoderUserOfferCard />
-      )}
+{      userOfferCards.map((user, index) => (
+  <UserOfferCard
+    key={index} // Use index as key if user.title is not unique
+    title={user.title}
+    description={user.description}
+    users={user.users}
+  />
+))}
     </div>
   );
 };
